e84ab19eea39ee0c77b580ba8cc6a54b2b6e9f64
Yuuki Endo <yuuki.endo@mymail.champlain.edu>
2025-04-30 07:40:15 -0400

    as

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { 
-    unoptimized: true 
-  },
-    basePath: "/yuukiendo.github.io/", // Your repository name
-    assetPrefix: "/yuukiendo.github.io/", // Note the trailing slash
+    output: "export",
+    images: {
+        unoptimized: true
+    },
+    // No basePath or assetPrefix needed for username.github.io repositories
 };
 
 export default nextConfig;
